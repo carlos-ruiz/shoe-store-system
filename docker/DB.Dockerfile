4ee23a42323c4e11cc5ae7685ac6067e62f46835
Carlos Ruiz <car.ruiz90@gmail.com>
2021-07-24 23:25:08 -0500

    Removing ignored files

--- docker/DB.Dockerfile
+++
@@ -1,8 +0,0 @@
-# Dockerfile-db
-
-# Use MySQL 5.7 for the base image
-FROM mariadb:latest
-
-# Copy database initialisation scripts
-COPY init.sql /docker-entrypoint-initdb.d/
-COPY database.sql /db/
--- docker/init.sql
+++
@@ -1,8 +0,0 @@
-# init.sql
-
-create database controlbom;
-use controlbom;
-source /db/database.sql;
-
-create user appuser identified by "appuserPasswd";
-grant all privileges on controlbom.* to appuser@'%';
